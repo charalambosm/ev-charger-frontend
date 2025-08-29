ce560a0e47c45c14cf1775e511272730e1cfb876
cmaxoutis79 <cmaxoutis79@gmail.com>
2025-08-29 10:33:00 +0300

    Adding iOS and Android package identifier

--- app.config.ts
+++ app.config.ts
@@ -1,28 +1,28 @@
 import type { ConfigContext, ExpoConfig } from "expo/config";
 export default ({ config }: ConfigContext): ExpoConfig => ({
-    name: config.name ?? "ev-charger-map-cy",
-    slug: config.slug ?? "ev-charger-map-cy",
-    ...config,
-    ios: {
-      ...(config.ios ?? {}),
-      bundleIdentifier: "com.yourname.evchargermapcy",
-      config: {
-        googleMapsApiKey: process.env.MAPS_IOS_KEY
-      },
-      infoPlist: {
-        NSLocationWhenInUseUsageDescription: "We use your location to show nearby chargers.",
-        ITSAppUsesNonExemptEncryption: false
-      }
+  name: config.name ?? "ev-charger-map-cy",
+  slug: config.slug ?? "ev-charger-map-cy",
+  ...config,
+  ios: {
+    ...(config.ios ?? {}),
+    bundleIdentifier: "com.evchargermapcy.cyprus",
+    config: {
+      googleMapsApiKey: process.env.MAPS_IOS_KEY
     },
-    android: {
-      ...(config.android ?? {}),
-      config: {
-        ...(config.android?.config ?? {}),
-        googleMaps: {
-          ...(config.android?.config?.googleMaps ?? {}),
-          apiKey: process.env.MAPS_ANDROID_KEY
-        }
-      }
+    infoPlist: {
+      NSLocationWhenInUseUsageDescription: "We use your location to show nearby chargers.",
+      ITSAppUsesNonExemptEncryption: false
     }
-  });
-  
+  },
+  android: {
+    ...(config.android ?? {}),
+    config: {
+      ...(config.android?.config ?? {}),
+      googleMaps: {
+        ...(config.android?.config?.googleMaps ?? {}),
+        apiKey: process.env.MAPS_ANDROID_KEY
+      },
+    },
+    package: "com.evchargermapcy.cyprus"
+  }
+});
